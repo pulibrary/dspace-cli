f9823c3e469b3861a40b07c18c64a0a7e31cd039
Monika Mevenkamp <monikam@princeton.edu>
2016-05-27 15:15:07 -0400

    print all eperson with their group memberships

--- netid/print.rb
+++ netid/print.rb
@@ -1,27 +1,9 @@
 #!/usr/bin/env jruby 
-require 'optparse'
 require 'dspace'
-
-options = {}
-parser = OptionParser.new do |opts|
-  opts.banner = "Usage: #{__FILE__}   netid.."
-end
-
-netids = ARGV
-if netids.empty? then
-   parser.help
-end
 
 DSpace.load
 
-netids.each do |netid| 
-    p = DEPerson.find(netid);
-    if (p) then
-      puts netid + ":"
-      DSpace.create(p).group_names.each { |n| puts "\t#{n}"}
-    else 
-       puts "ERROR: EPERSON.#{netid} does not exist"
-    end
-   puts "";
+DEPerson.all.each do |p|
+  props = DSpace.create(p).groups.collect { |g| "GROUP.#{g.getName}" }
+  puts "EPERSON.#{p.getEmail}\t" + props.join("\t")
 end
-
