cd74ae6333703546c5aecb4c768c3a72adf29ca9
Monika Mevenkamp <monikam@princeton.edu>
2016-05-27 21:34:50 -0400

    stats by submission and isued date

---
+++ statistics/bydate.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env jruby
+require 'dspace'
+
+DSpace.load
+
+puts ["Field", "Year", "Month", "#Items"].join("\t")
+['dc.date.accessioned', 'dc.date.issued'].each do |mdfield|
+  items= DItem.iter
+  submissions = {};
+  while (i = items.next()) do
+    date = i.getMetadata(mdfield)
+    begin
+      year, month = date.split('-')
+      submissions[year] ||= {'total' => 0}
+      submissions[year][month] ||= 0
+      submissions[year][month] += 1
+      submissions[year]['total'] += 1
+    rescue
+      puts "Item #{i} #{mdfield} value = '#{date}'"
+    end
+  end
+  submissions.keys.sort.each do |y|
+    submissions[y].keys.sort.each do |m|
+      puts [mdfield, y, m, submissions[y][m]].join("\t")
+    end
+  end
+end
---
+++ statistics/bydate_bycommunity.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env jruby
+require 'dspace'
+
+DSpace.load
+
+puts ["Field", "Year", "Month", "#Items"].join("\t")
+['dc.date.accessioned', 'dc.date.issued'].each do |mdfield|
+  submissions = {};
+  items = DItem.iter
+  while (i = items.next()) do
+    date = i.getMetadata(mdfield)
+    begin
+      year, month = date.split('-')
+      community = DSpace.create(i).parents[-1] # top level community
+      if (community and community.getHandle) then
+        ['total', community.getHandle].each do |bucket|
+          submissions[bucket] ||= {}
+          submissions[bucket][year] ||= {}
+          submissions[bucket][year][month] ||= 0
+          submissions[bucket][year][month] += 1
+          submissions[bucket][year]['total'] ||= 0
+          submissions[bucket][year]['total'] += 1
+          submissions[bucket]['total'] ||= {'total' => 0}
+          submissions[bucket]['total']['total'] += 1
+        end
+      else
+        $stderr.puts "Item #{i}  in community #{community} without a handle"
+      end
+    rescue
+      $stderr.puts "Item #{i} #{mdfield} value = '#{date}'"
+    end
+  end
+  print "\n"
+
+  puts ["Type", "Year", "Month", "#Items", "Community"].join("\t")
+  submissions.keys.sort.each do |bucket|
+    submissions[bucket].keys.sort.each do |year|
+      submissions[bucket][year].keys.sort.each do |month|
+        bname = DSpace.fromString(bucket) ? DSpace.fromString(bucket).getName : ''
+        puts [mdfield, year, month, submissions[bucket][year][month], bucket, bname].join("\t")
+      end
+    end
+  end
+end
