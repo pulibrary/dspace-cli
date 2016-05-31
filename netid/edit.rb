456f45244d0deb2c9f30f0d9731a0c68254f1223
Monika Mevenkamp <monikam@princeton.edu>
2016-05-31 10:11:37 -0400

    simplify netid/edit

--- netid/edit.rb
+++ netid/edit.rb
@@ -12,7 +12,7 @@ DSpace.load
 
 def print_members(p)
   puts p.getNetid + ":"
-  DSpace.create(p).group_names.each { |n| puts "\t#{n}" }
+  DSpace.create(p).groups.each { |g| puts "\t#{g.getName}" }
 end
 
 p = DEPerson.find(netid);
@@ -23,27 +23,27 @@ if (who.nil?) then
   who = ask "want to add to same groups as another user ? [return/netid] "
 end
 
-o = DEPerson.find(who);
-raise "no such eperson" if o.nil?
-print_members(o);
-pgroups = DSpace.create(p).groups
-DSpace.create(o).groups.each do |g|
-  if (pgroups.select { |pg| pg.getName() == g.getName }.empty?) then
-    yes = ask "add #{p} to #{g.getName()} ? [Y/N] ";
-    if (yes == "Y") then
-      puts "\tadding #{p} to #{g}"
-      g.addMember(p);
-      g.update();
+unless (who.empty?) then
+  o = DEPerson.find(who);
+  raise "no such eperson" if o.nil?
+  print_members(o);
+  pgroups = DSpace.create(p).groups
+  DSpace.create(o).groups.each do |g|
+    if (pgroups.select { |pg| pg.getName() == g.getName }.empty?) then
+      yes = ask "add #{p} to #{g.getName()} ? [Y/N] ";
+      if (yes == "Y") then
+        puts "\tadding #{p} to #{g}"
+        g.addMember(p);
+        g.update();
+      end
+    else
+      puts "\talready member of GROUP.#{g.getName}"
     end
-  else
-    puts "\talready member of #{g}"
   end
 end
-
 puts "";
 
-yes = ask "want to remove from groups ? [Y/N] "
-if (yes == "Y") then
+if "Y" == ask("want to remove from groups ? [Y/N] ") then
   DSpace.create(p).groups.each do |g|
     yes = ask "remove #{p} from #{g.getName()} ? [Y/N] ";
     if (yes == "Y") then
@@ -56,7 +56,6 @@ end
 puts "";
 
 print_members(p);
-yes = ask "want to commit the changes ? [Y/N] "
-if (yes == "Y") then
+if "Y" == ask("want to commit the changes ? [Y/N] ") then
   DSpace.commit
 end
