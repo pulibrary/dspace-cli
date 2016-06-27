34ae80013d12e92c3e806eccde6aff6e2ad7ae79
Monika Mevenkamp <monikam@princeton.edu>
2016-06-27 16:58:31 -0400

    fix DCollection copy

--- cli/dcollection.rb
+++ cli/dcollection.rb
@@ -65,7 +65,7 @@ class DCollection
   end
 
   def copy(name, parent)
-    raise "must give non nil parent community" if parent.nil? || parent.getType != DSpace::COMMUNITY
+    raise "must give non nil parent community" if parent.nil? || parent.getType != DConstants::COMMUNITY
     name = name.strip();
 
     puts "copying\t\t#{@obj} #{@obj.getName()}"
