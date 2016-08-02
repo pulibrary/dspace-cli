0fca56d62edbf0f11d150643f2bf7bb57bacb076
Monika Mevenkamp <monikam@princeton.edu>
2016-08-02 12:31:37 -0400

    print relevant group/collection info for s OAR/symplectic setup

--- symplectic/print_group_members.rb
+++ symplectic/print_group_members.rb
@@ -3,12 +3,21 @@ require "highline/import"
 require 'dspace'
 
 DSpace.load
-DSpace.login ENV['USER']
 puts "\n"
 
+puts ["Group    ", "Account Member's Email"].join("\t")
+DGroup.all.collect { |g| g.getName }.sort.each do |group_name|
+  group = DGroup.find(group_name)
+  group.getMembers.each do |member|
+    puts [group.getName, member.getEmail].join("\t")
+  end
+end
+puts ""
 
-DGroup.all.each do |group|
-  group.getMembers.each do |person|
-    puts [group.getName, person.getEmail].join(",")
+puts ["Group   ", "Member's Group Name"].join("\t")
+DGroup.all.collect { |g| g.getName }.sort.each do |group_name|
+  group = DGroup.find(group_name)
+  group.getMemberGroups.each do |member|
+    puts [group.getName, member.getName].join("\t")
   end
 end
