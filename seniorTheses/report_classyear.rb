8e21abdca9aaafea3a1caa1c12ac8de19951494c
Monika Mevenkamp <monikam@princeton.edu>
2016-08-25 13:12:13 -0400

    new seniorTheses/report_classyear

---
+++ seniorTheses/report_classyear.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env jruby  
+require 'xmlsimple'
+require 'dspace'
+
+DSpace.load
+
+def report(year)
+  puts ["Collection", "Year", "Status", "#Bistreams", "Formats", "Item", "Authors", "Advisors", "Title"].join("\t")
+
+  dsos = DSpace.findByMetadataValue('pu.date.classyear', year, DConstants::ITEM)
+  dsos.each do |d|
+    print_item(d)
+  end
+end
+
+def print_item(item)
+  authors = item.getMetadataByMetadataString("dc.contributor.author").collect { |v| v.value }.join","
+
+  advisors_plus =  item.getMetadataByMetadataString("dc.contributor.advisor").collect { |v| v.value }
+  advisors_plus += item.getMetadataByMetadataString("dc.contributor").collect { |v| v.value }
+  advisors_plus = advisors_plus.join ","
+
+  title = item.getName
+  year = item.getMetadataByMetadataString("pu.date.classyear").collect { |v| v.value }.join","
+
+  archived = item.isArchived ? "archived" :  "not-archived"
+  parent_name = item.getParentObject.nil? ? "" : item.getParentObject.getName
+  item_ref = item.getHandle.nil? ? "ITEM.#{item.getID}" : item.getHandle
+
+  bits = DSpace.create(item).bitstreams
+  formats = bits.collect { |b| b.getFormat.getMIMEType }.uniq.join(",")
+
+  vals =   [ parent_name, year, archived, bits.length, formats, item_ref, authors, advisors_plus, title]
+  puts vals.join("\t").gsub("\n", " " ).gsub("\r", " " )
+end
+
+
+report(2016)
