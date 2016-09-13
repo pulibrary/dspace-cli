d86a8b311854aa2f628d95ae9c164548ae350aa9
Monika Mevenkamp <monikam@princeton.edu>
2016-09-13 13:57:28 -0400

    def methods in netid/print.rb

--- netid/print.rb
+++ netid/print.rb
@@ -3,7 +3,14 @@ require 'dspace'
 
 DSpace.load
 
-DEPerson.all.each do |p|
+def print_all
+  DEPerson.all.each do |p|
+    pprint_netid p.getNetid
+  end
+end
+
+def print_netid netid
+  p = DEPerson.find(netid)
   props = DSpace.create(p).groups.collect { |g| "GROUP.#{g.getName}" }
   puts "EPERSON.#{p.getEmail}\t" + props.join("\t")
 end
