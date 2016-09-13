8a3a2e30c25dc3ca4552ae28564abac430bb9b65
Monika Mevenkamp <monikam@princeton.edu>
2016-09-13 13:58:35 -0400

    include symplectic ID in  symplectic/print_workflowitem_state.rb

--- symplectic/print_workflowitem_state.rb
+++ symplectic/print_workflowitem_state.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env jruby
 require 'dspace'
 require "cli/ditem.rb"
+require "symplectic/ditem.rb"
+
+DSpace.load
 
 def print_unarchived
-  java_import org.dspace.workflow.WorkflowItem;
+  java_import org.dspace.workflow.WorkflowItem
+  puts ["ITEM-ID", "Symplectic-ID", "state", "worflow", "department", "title"].join "\t"
   DItem.allUnarchived.each do |i|
     inflow = (WorkflowItem.findByItem(DSpace.context, i).nil? ? "not in" : "in") + " workflow"
     dept = i.getMetadataFirstValue('pu', 'author', 'department', nil) || 'department-not-crosswalked'
     value = i.getMetadataFirstValue('pu', 'workflow', 'state', nil) || 'UNDEFINED'
-    puts [i.toString, "pu.workflow.state=#{value}", inflow, dept, i.getName].join "\t"
+    puts [i.toString, DSpace.create(i).symplecticID, "pu.workflow.state=#{value}", inflow, dept, i.getName].join "\t"
   end
 end
 
