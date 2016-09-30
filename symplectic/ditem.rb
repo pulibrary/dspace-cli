50248a3fbbe727d51e7e5aac01f6516ebde94d36
Monika Mevenkamp <monikam@princeton.edu>
2016-09-30 15:07:15 -0400

    find DSpace item from symplectic id

--- symplectic/ditem.rb
+++ symplectic/ditem.rb
@@ -9,4 +9,13 @@ class DItem
     return tri.next().getStringColumn("PID")
   end
 
+  def self.findBySymplecticId sid
+    java_import org.dspace.storage.rdbms.DatabaseManager
+    sql = "SELECT ITEM_ID FROM SYMPLECTIC_PIDS WHERE PID = #{sid}";
+    tri = DatabaseManager.queryTable(DSpace.context, "SYMPLECTIC_PIDS", sql)
+    item_id = tri.next().getIntColumn("ITEM_ID")
+    puts item_id
+    DItem.find(item_id)
+  end
+
 end
