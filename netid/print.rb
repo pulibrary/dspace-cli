d63c922dd2cad8668dc6f68bb1686315a0674bf5
Monika Mevenkamp <monikam@princeton.edu>
2016-09-30 15:07:47 -0400

    netid/print.rb - tweak (print with or without group)

--- netid/print.rb
+++ netid/print.rb
@@ -4,13 +4,18 @@ require 'dspace'
 DSpace.load
 
 def print_all
-  DEPerson.all.each do |p|
-    pprint_netid p.getNetid
+  DEPerson.all.collect { |p| p.getEmail }.sort.each do |e|
+    print_netid e, false
   end
 end
 
-def print_netid netid
+def print_netid netid, groups
   p = DEPerson.find(netid)
-  props = DSpace.create(p).groups.collect { |g| "GROUP.#{g.getName}" }
-  puts "EPERSON.#{p.getEmail}\t" + props.join("\t")
+  if (groups) then
+    props = DSpace.create(p).groups.collect { |g| "GROUP.#{g.getName}" }
+  else
+    props = []
+  end
+  puts "EPERSON.#{p.getEmail}\t#{p.getNetid}\t" + props.join("\t")
 end
+
