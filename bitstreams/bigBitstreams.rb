2642b1f6507a2b26d1fd2f98e2e9ad62ac6d46b5
Monika Mevenkamp <monikam@princeton.edu>
2017-02-28 10:29:07 -0500

    delete -  def self.allWithoutMetadata() || add  def self.listBiggerThan(num)

--- bitstreams/bigBitstreams.rb
+++ bitstreams/bigBitstreams.rb
@@ -3,35 +3,35 @@ DSpace.load
 
 class DBitstream
 
-  def self.allWithoutMetadata()
+  def self.listBiggerThan(num)
     java_import org.dspace.storage.rdbms.DatabaseManager
-    sql = 'SELECT  DISTINCT(BITSTREAM_ID)  from BITSTREAM ' +
-          ' LEFT JOIN METADATAVALUE ' +
-          ' ON BITSTREAM.BITSTREAM_ID = METADATAVALUE.RESOURCE_ID ' +
-          ' WHERE METADATAVALUE.TEXT_VALUE  IS NULL AND METADATAVALUE.METADATA_FIELD_ID IS NULL'
+    sql = 'SELECT  BITSTREAM_ID, SIZE_BYTES from BITSTREAM '
       tri = DatabaseManager.queryTable(DSpace.context, "Bitstream", sql)
       dsos = [];
       while (iter = tri.next())
-        dsos << self.find(iter.getIntColumn("bitstream_id"))
+        s = iter.getLongColumn("SIZE_BYTES");
+        if (s >= num) then
+          dsos << self.find(iter.getIntColumn("bitstream_id"))
+          print_bitstream self.find(iter.getIntColumn("bitstream_id"))
+        end
       end
       tri.close
       return dsos
   end
 end
 
+def print_headers
+  puts ["SizeBytes", "Bitstream_ID", "handle", "BitstreamName"].join "\t"
+end
 def print_bitstream(d)
   item = d.getParentObject()
-  return if item.nil?
   handle = item.nil? ? 'no-handle' : item.getHandle
-  puts ([d.getID, handle, year] + DSpace.create(d).parents()).join("\t")
+  puts [d.getSize, d.getID, handle, d.getName].join("\t")
 end
 
-def doit()
-  dsos = DBitstream.allWithoutMetadata
-  dsos.each do |d|
-    print_bitstream(d)
-  end
-  dsos.length
+def doit(bigger)
+  print_headers
+  DBitstream.listBiggerThan(bigger)
 end
 
-doit()
+doit(1024 * 1024 *1024)
