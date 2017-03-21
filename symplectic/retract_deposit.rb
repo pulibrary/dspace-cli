3796a8aee64e128a7f87a3ca8423a346be919d23
Monika Mevenkamp <monikam@princeton.edu>
2017-03-21 14:24:53 -0400

    retract deposit script - interactively prompts for titles

--- symplectic/retract_deposit.rb
+++ symplectic/retract_deposit.rb
@@ -8,48 +8,56 @@ DSpace.context_renew
 DSpace.login ENV['USER']
 
 
-def doit(title)
-  items = DSpace.findByMetadataValue('dc.title', title, DConstants::ITEM)
-  raise 'multiple matched items' if items.length > 1
-  raise 'no matched items' if items.length < 1
-  item = items[0]
-
-  puts [item.getID, "archived:", item.isArchived, item.getName].join("\t")
-  puts "Workflow State " + item.getMetadataByMetadataString('pu.workflow.state').collect { |v| v.value }.join(", ")
-  di = DSpace.create(item)
-  di.retract
-  y = ask("commit changes ? (yes/no) ")
-  if (y == 'yes')
-    DSpace.commit
+def doit
+  while (true) do
+    puts "do not include speacial character in the title"
+    title = ask("title ")
+    return if title.empty?
+    do_item title.strip
   end
+
+end
+
+def do_item(title)
+  item = get_item(title)
+  puts ""
+
+  unless item.nil? then
+    if item.isArchived then
+      puts "ERROR: can't retract archived item"
+    else
+      symid = DSpace.create(item).symplecticID
+      puts "check in symplectic - should be deposited"
+      puts "/repository.html?pub=#{symid}"
+      puts "undeposit by removing the license - remember to impersonate the author"
+
+      yes = ask "return when undeposit done > "
+      puts "the item below should bbe the same as the one shown above"
+      item = get_item(title)
+      puts ""
+      yes = ask "retract ? (yes/no) "
+
+      if (yes == "yes") then
+        item.clearMetadata('pu', 'workflow', 'state', nil)
+        item.update
+        DSpace.commit
+        get_item(title)
+      end
+    end
+  end
+  DSpace.context_renew
+  DSpace.login ENV['USER']
 end
 
 def get_item(title)
+  item = DSpace.fromString "TITLE.#{title}%"
 
-end
-
-class DItem
-  java_import org.dspace.storage.rdbms.DatabaseManager
-
-  def retract()
-    unless dso.nil?
-      raise "can't retract archived item" if dso.isArchived
-      sql = "DELETE SYMPLECTIC_PIDS WHERE ITEM_ID = ?"
-      tri = DatabaseManager.updateQuery(DSpace.context, sql, dso.getID)
-      puts tri
-      wflows = DWorkflowItem.findAll(dso)
-      wflows.each do |wf|
-        wf.deleteWrapper
-      end
-      dso.delete
-    end
+  if item.nil? then
+    puts ["n/a", "title:", "no match"].join("\t")
+  else
+    puts [item.getID, "title:", title].join("\t")
+    puts [item.getID, "archived:", item.isArchived, item.getName].join("\t")
+    puts "pu.workflow.state" + item.getMetadataByMetadataString('pu.workflow.state').collect { |v| v.value }.join(", ")
   end
-
+  item
 end
-
-
-# title = "A Transport Model for Estimating the Time Course of ERK Activation in the C%"
-puts "do not include speacial character in the title"
-title = ask("title ") unless title
-puts "Title: " + title
-doit title
