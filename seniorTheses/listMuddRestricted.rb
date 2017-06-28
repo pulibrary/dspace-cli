5d4cc189a9c5b94532e4cb817d6f53c085f89b36
Monika Mevenkamp <monikam@princeton.edu>
2017-06-28 12:08:12 -0400

    also look for SrTHesisAnonymous - just in case

--- seniorTheses/listMuddRestricted.rb
+++ seniorTheses/listMuddRestricted.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env jruby
 require 'dspace'
+DSpace.load
 
 puts ["Handle", "Year", "READ_restriction",  "Bitstream Name",].join "\t"
 
-["SrTheses_Bitstream_Read_Mudd", "Princeton_Mudd_IPs"].each do |group_name|
+["SrTheses_Bitstream_Read_Mudd", "SrTheses_Bitstream_Read_Princeton", "SrTheses_Item_Read_Anonymous"].each do |group_name|
   mudds = DSpace.findByGroupPolicy(group_name, DConstants::READ, DConstants::BITSTREAM)
   mudds.each do |b|
     i = b.getParentObject
