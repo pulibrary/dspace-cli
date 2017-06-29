379d90e81388b33010edb57ff2ce0087090c769d
Monika Mevenkamp <monikam@princeton.edu>
2017-06-29 14:58:41 -0400

    forever tweaking the bitstream lister

--- seniorTheses/listBitstreams.rb
+++ seniorTheses/listBitstreams.rb
@@ -15,7 +15,8 @@ fromString = '88435/dsp019c67wm88m'
 com = DSpace.fromString(fromString)
 
 def year_csv(year)
-  size, handle, col, fname, klass, elift, eterm, policies = ['filesize MB', 'handle', 'collection', 'filename', 'year', 'lift', 'term', "policies..."]
+  size, item_id, bitstream_id, handle, col, fname, klass, elift, eterm, walkin, walk_msg, policies =
+      ['filesize MB', 'item_id', 'bitstream_id', 'handle', 'collection', 'filename', 'year', 'lift', 'term', 'walkin', 'rights_msg', "policies..."]
 
   items = DSpace.findByMetadataValue('pu.date.classyear', year, nil)
   handles = items.select{ |i| i.getHandle }.collect{ |i| i.getHandle }
@@ -24,12 +25,16 @@ def year_csv(year)
   handles.each do |h|
     i = DSpace.fromString h
     DSpace.create(i).bitstreams.each do |b|
-      h =  report_on(b, fname, size, policies)
+      h =  report_on(b, bitstream_id, fname, size, policies)
       h[klass] = year
+      h[item_id] = i.getID
       h[handle] = i.getHandle
       h[col] = i.getParentObject.getName
-      h[elift] = (nil == i.getMetadata('pu.embargo.lift')) ? '----' : i.getMetadata('pu.embargo.lift')
-      h[eterm] = (nil == i.getMetadata('pu.embargo.terms')) ? '----' : i.getMetadata('pu.embargo.terms')
+      h[walk_msg] = (nil == i.getMetadata('dc.description')) ? ':::' : i.getMetadata('dc.description')
+      h[walk_msg] = h[walk_msg][0..10]
+      h[walkin] = (nil == i.getMetadata('pu.mudd.walkin')) ? '---' : i.getMetadata('pu.mudd.walkin')
+      h[elift] = (nil == i.getMetadata('pu.embargo.lift')) ? '---- -- --' : i.getMetadata('pu.embargo.lift')
+      h[eterm] = (nil == i.getMetadata('pu.embargo.terms')) ? '---- -- --' : i.getMetadata('pu.embargo.terms')
       ihash << h
       break
     end
@@ -40,11 +45,12 @@ def year_csv(year)
     end
   end
 
-  csv_out(ihash, [size, klass, handle, col, fname, elift, eterm, policies])
+  csv_out(ihash, [item_id, klass, handle, elift, eterm, walkin, walk_msg, policies])
 end
 
-def report_on(b, fname, size, policies)
+def report_on(b, id, fname, size, policies)
   h = {}
+  h[id] = b.getID
   h[fname] = b.getName
   h[size] = (1.0 * b.getSize) / (1024 * 1024)
   h[policies] = get_policies(b)
@@ -61,7 +67,7 @@ def get_policies(b)
     person = DEPerson.find iter.getIntColumn("EPERSON_ID")
     group = DGroup.find iter.getIntColumn("EPERSONGROUP_ID")
     group = group.getName if group
-    pols << [action, person, group]
+    pols << [action, person, group].join(",")
   end
   tri.close()
   return pols
@@ -77,7 +83,7 @@ end
 
 if true then
   #year_csv(2014)
-  year_csv(2015)
+  year_csv(2020)
 end
 
 
