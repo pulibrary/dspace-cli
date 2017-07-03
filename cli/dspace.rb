afe108b55057134e12715664b5d064078cb8aa65
Monika Mevenkamp <monikam@princeton.edu>
2017-07-03 13:52:38 -0400

    add DSpace.embargoed convenience method

--- cli/dspace.rb
+++ cli/dspace.rb
@@ -7,4 +7,8 @@ module DSpace
       puts "ark.shoulder  #{DConfig.get('ark.shoulder')}"
     end
   end
+
+  def self.embargoed()
+    items = DSpace.findByMetadataValue('pu.embargo.lift', nil, nil)
+  end
 end
