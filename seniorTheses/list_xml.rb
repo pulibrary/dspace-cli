1dec0d87efc1595786b0d3fd5a8bfbfa1a307303
Monika Mevenkamp <monikam@princeton.edu>
2017-09-27 13:12:25 -0400

    add  seniorTheses/list_xml.rb

---
+++ seniorTheses/list_xml.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env jruby  
+require 'xmlsimple'
+require 'dspace'
+
+DSpace.load
+
+# dataspace
+fromString = '88435/dsp019c67wm88m'
+com = DSpace.fromString(fromString)
+
+def all_year_hsh(year)
+  items = DSpace.findByMetadataValue('pu.date.classyear', year, nil)
+  cols = {};
+  items.each do |i|
+    if (i.getHandle) then
+      h = {}
+      h[:title] = i.getMetadataByMetadataString("dc.title").collect { |v| v.value }
+      h[:author] = i.getMetadataByMetadataString("dc.contributor.author").collect { |v| v.value }
+      h[:advisor] = i.getMetadataByMetadataString("dc.contributor.advisor").collect { |v| v.value }
+      h[:classyear] = i.getMetadataByMetadataString("pu.date.classyear").collect { |v| v.value }
+      h[:department] = i.getMetadataByMetadataString("pu.department").collect { |v| v.value }
+      h[:url] = i.getMetadataByMetadataString("dc.identifier.uri").collect { |v| v.value }
+      cols[i.getParentObject] = [] unless cols[i.getParentObject]
+      cols[i.getParentObject] << h
+    end
+  end
+  return cols
+end
+
+def col_hsh_print(hsh)
+  hsh.keys.each do |col|
+    ihash = hsh[col]
+    colurl = "http://arks.princeton.edu/ark:/#{col.getHandle()}"
+    File.open(col.toString + ".xml", 'w') do |out|
+      out.puts XmlSimple.xml_out({:name => col, :url => colurl, :item => ihash}, :root_name => 'collection')
+    end
+  end
+end
+
+def all_xml_year(year)
+  col_hsh_print(all_year_hsh(year))
+end
+
+all_xml_year(2010) 
