b541d1853a313e6a974fdfdd9a554ac85e28166f
Monika Mevenkamp <monikam@princeton.edu>
2018-04-30 15:23:16 -0400

    create new collection in Senior Thesis community - does not set coll-authorizations

---
+++ seniorTheses/listNoBitstreams.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env jruby
+require 'dspace'
+DSpace.load
+
+puts ["Handle", "Year", "READ_restriction",  "Bitstream Name",].join "\t"
+
+["SrTheses_Bitstream_Read_Mudd", "SrTheses_Bitstream_Read_Princeton", "SrTheses_Item_Read_Anonymous"].each do |group_name|
+  mudds = DSpace.findByGroupPolicy(group_name, DConstants::READ, DConstants::BITSTREAM)
+  mudds.each do |b|
+    i = b.getParentObject
+    year = i.getMetadataByMetadataString("pu.date.classyear")[0].value.to_i
+    if (year > 2015) then
+      puts [i.getHandle, year, group_name, b.getName].join "\t"
+    end
+  end
+
+end
+
