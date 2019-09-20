b5ff3518eeee242bf486bba01ace402513fd871a
Monika Mevenkamp <monikam@princeton.edu>
2019-09-20 09:54:30 -0400

    cleanup_import_errors.rb for 2019

--- thesis_central/cleanup_import_errors.rb
+++ thesis_central/cleanup_import_errors.rb
@@ -1,26 +1,54 @@
 require 'cli'
+require 'setup'
 
 DSpace.load
 DSpace.login 'monikam'
 
 dept = 'Creative Writing Program'
-dept = 'Aeronautical Engineering'
 dept = 'Theater'
+dept = 'Aeronautical Engineering'
 
 def del_bad_department(dept)
   items = DSpace.findByMetadataValue('pu.department',  dept, 2)
   for i in items do
      if not i.getOwningCollection.getName.start_with? dept then
        shortName = i.getOwningCollection.getName.split(',')[0]
-       if shortName.start_with? 'Neuro' then
-         puts ['NEURO', i.getHandle, shortName, i.getOwningCollection.getName].join  "\t"
-       else
-         puts ['DELETE', i.getHandle, shortName, i.getOwningCollection.getName, i.getMetadataFirstValue('pu', 'certificate', nil, '*')].join  "\t"
-         #i.clearMetadata('pu', 'department', nil, '*')
-         #i.update
+       if (shortName == 'Creative Writing Program') then
+        puts [i.getID, i.getHandle, i.getMetadataFirstValue('pu', 'date', 'classyear', '*'),
+              shortName, i.getOwningCollection.getName,
+              i.getMetadataFirstValue('pu', 'department', nil, '*'),
+              i.getMetadataFirstValue('pu', 'certificate', nil, '*')].join  "\t"
+        i.clearMetadata('pu', 'department', nil, '*')
+        i.update
        end
+
      end
   end
+  return items
 end
 
+
+def fix_bad_department(dept)
+  items = DSpace.findByMetadataValue('pu.department',  dept, 2)
+  for i in items do
+    if not i.getOwningCollection.getName.start_with? dept then
+      shortName = i.getOwningCollection.getName.split(',')[0]
+      if (shortName != 'Creative Writing Program' ) then
+        puts [i.getID, i.getHandle, i.getMetadataFirstValue('pu', 'date', 'classyear', '*'),
+              'desired= ' + shortName, 'col-name=' + i.getOwningCollection.getName,
+              'pu.deprtment=' + i.getMetadataFirstValue('pu', 'department', nil, '*')
+              ].join  "\t"
+        i.setMetadataSingleValue('pu', 'department', nil, '*', shortName)
+        i.update
+      end
+
+    end
+  end
+  return items
+end
+
+
 del_bad_department dept
+fix_bad_department dept
+#DSpace.commit
+
