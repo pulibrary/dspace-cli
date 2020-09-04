5a2ad8ce5cd0694d7847b008bed6d65d4c5703b0
James R. Griffin III <jrgriffiniii@gmail.com>
2020-09-04 00:37:16 -0400

    Adding the functionality for adding and removing users from workflow task pools

--- dspace/cli/senior_thesis_community.rb
+++ dspace/cli/senior_thesis_community.rb
@@ -22,6 +22,9 @@ module DSpace
     end
 
     class SeniorThesisWorkflowItem
+      java_import org.dspace.storage.rdbms.DatabaseManager
+      java_import org.dspace.eperson.EPerson
+
       attr_reader :obj
 
       def self.kernel
@@ -33,10 +36,67 @@ module DSpace
         @obj = obj
       end
 
+      def id
+        @obj.getID
+      end
+
       def delete
         @obj.deleteWrapper
         self.class.kernel.commit
         @obj = nil
+      end
+
+      def self.delete_query
+        "DELETE FROM tasklistitem WHERE eperson_id = ? AND workflow_id = ?"
+      end
+
+      def self.delete_from_database(eperson_id, workflow_id)
+        Java::OrgDspaceStorageRdbms::DatabaseManager.updateQuery(kernel.context, delete_query, eperson_id.to_java, workflow_id.to_java)
+        kernel.commit
+      end
+
+      # magical task pool population
+      def create_workflow_tasks(epeople)
+        epeople.each do |eperson|
+          table_row = Java::OrgDspaceStorageRdbms::DatabaseManager.row("tasklistitem")
+          table_row.setColumn("eperson_id", eperson.getID)
+          table_row.setColumn("workflow_id", id)
+          Java::OrgDspaceStorageRdbms::DatabaseManager.insert(self.class.kernel.context, table_row)
+
+          self.class.kernel.commit
+        end
+      end
+
+      def delete_workflow_tasks(epeople)
+        epeople.each do |eperson|
+          self.class.delete_from_database(eperson.getID, id)
+        end
+      end
+
+      def remove_task_pool_users(emails)
+        users = emails.map do |email|
+          Java::OrgDspaceEperson::EPerson.findByEmail(self.class.kernel.context, email)
+        end
+        epeople = users.reject(&:nil?)
+
+        delete_workflow_tasks(epeople)
+      end
+
+      def remove_task_pool_user(email)
+        remove_task_pool_users([email])
+      end
+
+      def add_task_pool_users(emails)
+        users = emails.map do |email|
+          Java::OrgDspaceEperson::EPerson.findByEmail(self.class.kernel.context, email)
+        end
+        epeople = users.reject(&:nil?)
+
+        create_workflow_tasks(epeople)
+      end
+
+      def add_task_pool_user(email)
+        add_task_pool_users([email])
       end
     end
 
@@ -389,6 +449,7 @@ module DSpace
         self.class.workflow_item_class.new(workflow_obj)
       end
 
+      
       def collections
         @obj.getCollections.map { |collection_obj| self.class.collection_class.new(collection_obj) }
       end
